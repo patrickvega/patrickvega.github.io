86a028f7fde311d29535fefa7e94fe12aa9422c6
Patrick Vega <patrickzao@gmail.com>
2020-09-09 15:33:48 +0200

    Personal website
    Personal website

---
+++ js/frames.js
@@ -0,0 +1,46 @@
+   
+function frames(variable){
+    document.getElementById("enframe").src="pages/home.html";
+    if (variable==1){
+        document.getElementById("enframe").src="pages/home.html";
+    }
+    else if (variable==2){
+        document.getElementById("enframe").src="pages/submitted.html";
+    }
+    else if (variable==3){
+        document.getElementById("enframe").src="pages/published.html";
+    }
+    else if (variable==4){
+        document.getElementById("enframe").src="pages/my_thesis.html";
+    }
+    else if (variable==5){
+        document.getElementById("enframe").src="pages/about_me.html";
+    }
+    else if (variable==6){
+        document.getElementById("enframe").src="pages/2019.html";
+    }   
+    else if (variable==7){
+        document.getElementById("enframe").src="pages/2018.html";
+    }     
+    else if (variable==8){
+        document.getElementById("enframe").src="pages/2017.html";
+    }  
+    else if (variable==9){
+        document.getElementById("enframe").src="pages/2016.html";
+    }  
+    else if (variable==10){
+        document.getElementById("enframe").src="pages/presentations/presentations_2018.html";
+    }  
+    else if (variable==11){
+        document.getElementById("enframe").src="pages/distinctions.html";
+    } 
+    else if (variable==12){
+        document.getElementById("enframe").src="pages/relevant-duties.html";
+    }    
+    else if (variable==13){
+        document.getElementById("enframe").src="pages/linux-software.html";
+    }  
+    else if (variable==14){
+        document.getElementById("enframe").src="pages/links.html";
+    }  
+} 
